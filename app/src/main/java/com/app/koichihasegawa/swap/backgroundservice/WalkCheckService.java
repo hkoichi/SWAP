1ac4ea980280a80a7bbb2bf29e2b0835d9686196
Koichi Hasegawa <>
2017-11-09 16:36:21 +0900

    add walking check

--- app/src/main/java/com/app/koichihasegawa/swap/backgroundservice/WalkCheckService.java
+++ app/src/main/java/com/app/koichihasegawa/swap/backgroundservice/WalkCheckService.java
@@ -21,16 +21,14 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
-import org.opencv.core.Point;
 import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 import java.io.IOException;
-import java.util.Locale;
 
+import static android.hardware.SensorManager.SENSOR_DELAY_FASTEST;
 import static org.opencv.android.Utils.bitmapToMat;
 import static org.opencv.android.Utils.matToBitmap;
 
@@ -40,7 +38,7 @@ import static org.opencv.android.Utils.matToBitmap;
  */
 
 public class WalkCheckService extends Service {
-    public static Camera camera = null;
+    private Camera camera = null;
     private static final String TAG = "OCVSample::Activity";
     private SurfaceTexture mSurfaceTexture = new SurfaceTexture(10);
 
@@ -54,18 +52,21 @@ public class WalkCheckService extends Service {
     private Size mMinFaceSize = new Size(0, 0);
     private Size mMaxFaceSize = new Size(0, 0);
 
+    private boolean isPhoning = false;
+    private boolean isSWAPed = false;
+    private final int alivePhoning = 3;
     private int detectNum = 0;
-    private float steps = 0;
 
     private SensorManager mSensorManager;
     private Sensor mStepDetectorSensor;
-    private Sensor mStepConnterSensor;
-
 
     // スレッドを制御する変数
-    private boolean runnning = true;
+    private boolean isRunnning = true;
+    private Thread processingThread;
     // cascade classifierを用いた顔認証をするインスタンス
     private CascadeClassifier mFaceDetector;
+
+    public static Bitmap bmp;
 
     // opencv が読み込まれた後に呼ばれるコールバック
     BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(WalkCheckService.this) {
@@ -88,22 +89,19 @@ public class WalkCheckService extends Service {
 
     // 画像処理スレッドの呼び出し
     public void startImageProcessingThread() {
-        Thread thread = new Thread() {
+        processingThread = new Thread() {
             public void run() {
                 camera = Camera.open(1);
                 camera.setPreviewCallback(new Camera.PreviewCallback() {
                     @Override
                     public void onPreviewFrame(byte[] data, Camera camera) {
-                        if (!runnning) {
-                            return;
-                        }
                         if (cameraWidth == 0) {
                             parameters = camera.getParameters();
                             cameraWidth = parameters.getPreviewSize().width;
                             cameraHeight = parameters.getPreviewSize().height;
                         }
                         // frame からbitmapの作成
-                        Bitmap bmp = Utils.makeBitmap(data, cameraWidth, cameraHeight, parameters);
+                        bmp = Utils.makeBitmap(data, cameraWidth, cameraHeight, parameters);
                         // bitmap からmatの作成
                         Mat oldMat = new Mat();
                         bitmapToMat(bmp, oldMat);
@@ -112,7 +110,7 @@ public class WalkCheckService extends Service {
                         // matからbitmapの作成
                         matToBitmap(newMat, bmp);
                         // 反映
-                        MainActivity.imageView.setImageBitmap(bmp);
+//                        MainActivity.imageView.setImageBitmap(bmp);
                     }
                 });
                 try {
@@ -123,7 +121,7 @@ public class WalkCheckService extends Service {
                 camera.startPreview();
             }
         };
-        thread.start();
+        processingThread.start();
     }
 
     // 画像処理
@@ -149,19 +147,17 @@ public class WalkCheckService extends Service {
                 mMaxFaceSize);
         Rect[] facesArray = faces.toArray();
         if (facesArray.length > 0) {
-            detectNum++;
-            if (detectNum > 15) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("isStop", true);
-                this.startActivity(intent);
+            detectNum = alivePhoning;
+            isPhoning = true;
+            Log.d("phone", "watching");
+        } else {
+            detectNum--;
+            if (detectNum < 0) {
+                isPhoning = false;
+                Log.d("phone", "not watching");
+            } else {
+                Log.d("phone", "watching");
             }
-            MainActivity.textView.setText(Integer.toString(detectNum));
-            Imgproc.rectangle(oldMat,
-                    new Point(facesArray[0].tl().x * SCALE, facesArray[0].tl().y * SCALE),
-                    new Point(facesArray[0].br().x * SCALE, facesArray[0].br().y * SCALE),
-                    new Scalar(1, 1, 0, 1), 3);
         }
         return oldMat;
     }
@@ -169,25 +165,28 @@ public class WalkCheckService extends Service {
 
     @Override
     public void onCreate() {
-        //センサーマネージャを取得
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        //センサマネージャから TYPE_STEP_DETECTOR についての情報を取得する
         mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-
-        //センサマネージャから TYPE_STEP_COUNTER についての情報を取得する
-        mStepConnterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-
-        setStepCounterListener();
-
+        mSensorManager.registerListener(mStepDetectListener, mStepDetectorSensor, SENSOR_DELAY_FASTEST);
         super.onCreate();
     }
 
-    private final SensorEventListener mStepCountListener = new SensorEventListener() {
+    private final SensorEventListener mStepDetectListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
-            steps = sensorEvent.values[0];
-            MainActivity.textViewStep.setText(String.format(Locale.US, "%f", steps));
+            Log.d("walking", "true");
+            if (isPhoning && !isSWAPed) {
+                Log.d("SWAP", "ダメよ-");
+                MainActivity.imageView.setImageBitmap(bmp);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("isStop", true);
+                getApplication().startActivity(intent);
+                isSWAPed = true;
+            } else {
+                Log.d("SWAP", "みてないからおk！");
+            }
         }
 
         @Override
@@ -196,21 +195,13 @@ public class WalkCheckService extends Service {
         }
     };
 
-    private void setStepCounterListener() {
-        if (mStepConnterSensor != null) {
-            //ここでセンサーリスナーを登録する
-            mSensorManager.registerListener(mStepCountListener, mStepConnterSensor, SensorManager.SENSOR_DELAY_FASTEST);
-        }
-    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // opencv のロード
         if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
         } else {
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
         return super.onStartCommand(intent, flags, startId);
@@ -219,11 +210,12 @@ public class WalkCheckService extends Service {
 
     @Override
     public void onDestroy() {
-        runnning = false;
+        isRunnning = false;
         mSurfaceTexture.detachFromGLContext();
         mSurfaceTexture.release();
         camera.stopPreview();
         camera.lock();
+        camera.setPreviewCallback(null);
         camera = null;
     }
 
--- app/src/main/java/com/app/koichihasegawa/swap/ui/MainActivity.java
+++ app/src/main/java/com/app/koichihasegawa/swap/ui/MainActivity.java
@@ -4,25 +4,17 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.app.koichihasegawa.swap.R;
 import com.app.koichihasegawa.swap.backgroundservice.WalkCheckService;
 import com.app.koichihasegawa.swap.databinding.ActivityMainBinding;
 
-import static com.app.koichihasegawa.swap.backgroundservice.WalkCheckService.camera;
 
-
-public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
-    public static ImageView imageView;
-    public static TextView textView;
-    public static TextView textViewStep;
-
+public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    public static ImageView imageView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,56 +22,42 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setActivity(this);
+        imageView = binding.imageView;
 
         Intent i = getIntent();
         boolean isStop = i.getBooleanExtra("isStop", false);
         if (isStop) {
             stopService(new Intent(MainActivity.this, WalkCheckService.class));
+            imageView.setImageBitmap(WalkCheckService.bmp);
         }
-
-        imageView = binding.imageView;
-        textView = binding.textView;
-        textViewStep = binding.textViewStep;
-
     }
 
     public void onClickStart(View v) {
         Intent intent = new Intent(MainActivity.this, WalkCheckService.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startService(intent);
-        Toast.makeText(getApplicationContext(), "start checking", Toast.LENGTH_SHORT).show();
         v.setVisibility(View.GONE);
         v.setClickable(false);
         binding.StopService.setVisibility(View.VISIBLE);
         binding.StopService.setClickable(true);
+        binding.explainText.setVisibility(View.GONE);
+        binding.progressBar.setVisibility(View.GONE);
+        binding.progressBar.setVisibility(View.VISIBLE);
     }
 
     public void onClickStop(View v) {
         stopService(new Intent(MainActivity.this, WalkCheckService.class));
-        Toast.makeText(getApplicationContext(), "stop checking", Toast.LENGTH_SHORT).show();
         v.setVisibility(View.GONE);
         v.setClickable(false);
         binding.StartService.setVisibility(View.VISIBLE);
         binding.StartService.setClickable(true);
+        binding.explainText.setVisibility(View.VISIBLE);
+        binding.progressBar.setVisibility(View.GONE);
     }
 
     @Override
     protected void onDestroy() {
-        camera.release();
+        stopService(new Intent(MainActivity.this, WalkCheckService.class));
         super.onDestroy();
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // TODO Auto-generated method stub
-
     }
 }
